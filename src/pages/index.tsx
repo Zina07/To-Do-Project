b82742680e6eb2e78a1e3a9a05ef4b1f6a83dc91
Zina A <keiraayari@gmail.com>
2024-04-11 10:30:51 +0200

    setup nextjs, talwind css

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home2() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello World, Je suis là TO DO Project arrive !!!
+      </h1>
+    )
+  }
