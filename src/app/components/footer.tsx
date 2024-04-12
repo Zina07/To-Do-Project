144700501b8449740f79b7f571370bd24a9cc3cc
Zina A <keiraayari@gmail.com>
2024-04-12 10:41:07 +0200

    rajout dans l'arbo component & pages en wip

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+import {footer} from "footer"
+
+export default function footer() {
+    return (
+      <p className="text-3xl font-bold underline">
+       Copyright Zina 2024
+      </p>
+    )
+  }
