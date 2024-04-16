6bfe79cc623b3b72e5940f74d08c046372dad58b
Zina A <keiraayari@gmail.com>
2024-04-16 12:45:59 +0200

    correction footer ok

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,43 +4,10 @@ export default function Footer() {
       <div className="bg-gradient-to-r from-gray-500 ...">
         <div className="divide-y-8 border-top-width: 8px border-bottom-width: 0px;">
           <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div className="sm:flex sm:items-center sm:justify-between">
-              {/* <a
-            href="https://flowbite.com/"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
-          </a> */}
+            <div className="sm:flex sm:items-center sm:justify-between"></div>
 
-              {/* <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                {/* <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li> 
-            </ul> */}
-            </div>
-            {/* <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" /> */}
             <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2024{" "}
-              {/* <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
-          </a> */}
-              Zina{" "}
+              © 2024 Zina
             </span>
           </div>
         </div>
