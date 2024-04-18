de12168379903ee834fd1b7c99250e1983ad45d6
Zina A <keiraayari@gmail.com>
2024-04-18 10:02:36 +0200

    nettoyage code compoment todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -28,39 +28,7 @@ export default function Todo() {
         </ul>
       </div>
 
-      {/* <input
-            id="link-checkbox"
-            type="checkbox"
-            value=""
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="link-checkbox"
-            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            {" "} */}
-      {/* <a
-              href="#"
-              className="text-blue-600 dark:text-blue-500 hover:underline"
-            >
-              terms and conditions
-            </a> */}
-      {/* </label> */}
-      {/* <!-- Input: Designation [h-12] & min-w-[12rem] --> */}
-      {/* <input
-            className="h-12 min-w-[12rem] rounded-lg border-emerald-500 indent-4 text-emerald-900 shadow-lg focus:outline-none focus:ring focus:ring-emerald-600"
-            type="text"
-            placeholder="Designation"
-          />
-          {/* <!-- Button: Submit [h-12] --> */}
-      {/* <button
-            id="annul"
-            className="h-12 min-w-[8rem] rounded-lg border-2 border-emerald-600 bg-emerald-500 text-emerald-50 shadow-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-emerald-600"
-          >
-            Submit
-          </button> */}
-
-      {/* </section> */}
+     
     </div>
   );
 }
