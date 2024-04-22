5c58f7b7537d4c7bfbaad825f915b3831289f5be
Zina A <keiraayari@gmail.com>
2024-04-22 13:08:11 +0200

    ajout cadre dans les items en wip

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 export default function Item({ numero, title }) {
   return (
     <div className="">
-      {numero} - {title}
+      {numero} - {title} 
     </div>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -30,12 +30,52 @@ export default function Todo() {
             Ajouter
           </button>
         </div>
+
+        {/* =============================== Liste avec Numero + Title =  désignation de la tache  + Checkbox ==================*/}
         <div className="">
           <h3> liste des tâches</h3>
-          <Item numero={1} title="la tache"/>
-          <Item  title="acheter une pomme"/>
-          <Item  title="la tache"/>
-          <Item  title="la tache"/>
+
+          <div
+            className="mb-6 bg-green-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+            focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 
+            dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <div className="flex items-center">
+              <Item numero={1} title="la tache" />
+              <input
+                id="checked-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              ></input>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <Item numero={2} title="acheter une pomme" />{" "}
+            <input
+              id="checked-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            ></input>
+          </div>
+          <div className="flex items-center">
+            <Item numero={3} title="la tâche" />{" "}
+            <input
+              id="checked-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            ></input>
+          </div>
+          <div className="flex items-center">
+            <Item numero={4} title="la tâche" />{" "}
+            <input
+              id="checked-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            ></input>
+          </div>
         </div>
       </div>
     </div>
